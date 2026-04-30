01330f14c98ffdf7d9ec156c37992fc8d945d812
PraTaaMa <rockypratama4@gmail.com>
2026-04-30 11:17:03 +0700

    pembuatan tampilan homepage dan login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::');
+$routes->get('', 'Home::login');
 $routes->get('crud', 'Crud::index');
 $routes->get('crud-create', 'Crud::create');
 $routes->post('crud-create', 'Crud::save');
- 
+$routes->get('login', 'Home::login');  
